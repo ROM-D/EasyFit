465069e9c187b498dfb94ace5bc2a5db9bac2b6e
SpacecOOk <a01748632@itesm.mx>
2021-10-25 22:56:04 -0500

    Mich-del login a la navegacion (falta el sign out y unir a bd)

--- app/src/main/java/mx/madg/easyfit/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/madg/easyfit/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package mx.madg.easyfit.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
 import mx.madg.easyfit.R
 import mx.madg.easyfit.databinding.FragmentHomeBinding
+import mx.madg.easyfit.loginActivity
 
 class HomeFragment : Fragment() {
 
@@ -31,11 +34,22 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+
+
+        val textView: TextView = binding.textView10
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+           textView.text = it
+       })
+        configurarEventos()
+
         return root
+    }
+
+    private fun configurarEventos() {
+        binding.btnSignout.setOnClickListener {
+            AuthUI.getInstance().signOut()
+            startActivity(Intent(activity,loginActivity::class.java))
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/mx/madg/easyfit/ui/login/NavLoginViewModel.kt
+++
@@ -1,7 +0,0 @@
-package mx.madg.easyfit.ui.login
-
-import androidx.lifecycle.ViewModel
-
-class NavLoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
