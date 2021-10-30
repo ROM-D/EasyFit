1ddee7eb6a812ca6f633c5a60b2192139b9413b3
Diego Elizalde <a01748756@itesm.mx>
2021-10-30 13:41:51 -0500

    Se implemento el perfil del cliente y actualizado con bd

--- app/src/main/java/mx/madg/easyfit/ui/Cliente.kt
+++ app/src/main/java/mx/madg/easyfit/ui/Cliente.kt
@@ -1,9 +1,9 @@
 package mx.madg.easyfit.ui
 
 data class Cliente(
-    var id:String,
-    var nombre:String?,
-    var mail:String?,
+    var id:String? = "",
+    var nombre:String? = "",
+    var mail:String? = "",
     var peso:Double=0.0,
     var altura:Double=0.0,
     var sexo:String="",
--- app/src/main/java/mx/madg/easyfit/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/madg/easyfit/ui/home/HomeFragment.kt
@@ -11,9 +11,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import mx.madg.easyfit.R
 import mx.madg.easyfit.databinding.FragmentHomeBinding
 import mx.madg.easyfit.loginActivity
+import mx.madg.easyfit.ui.Cliente
 
 class HomeFragment : Fragment() {
 
@@ -36,13 +43,53 @@ class HomeFragment : Fragment() {
 
 
 
-        val textView: TextView = binding.textView10
+        //val textView: TextView = binding.textView10
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-           textView.text = it
+           //textView.text = it
        })
         configurarEventos()
 
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        leerDatos()
+    }
+
+    private fun leerDatos() {
+        val baseDatos = Firebase.database
+        val usuario = FirebaseAuth.getInstance().currentUser
+
+        //Clientes
+        val referencia = baseDatos.getReference("Clientes/${usuario?.uid}")
+        //println("LA REFERENCIA: ${referencia}")
+
+        //Se actualiza cada vez que me llegue un dato
+        referencia.addValueEventListener(object: ValueEventListener { //Este me da avisos o actualizaciones en tiempo real
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //println("LA REFERENCIA: ${referencia}")
+                //println("EL SNAPSHOT: ${snapshot}")
+                val cliente =snapshot.getValue(Cliente::class.java)
+                binding.tvNombre.setText(cliente?.nombre.toString())
+                val id = "id: "+cliente?.id.toString()
+                binding.tvId.setText(id)
+                binding.tvEmail.setText(cliente?.mail.toString())
+                binding.tvPeso.setText(cliente?.peso.toString())
+                binding.tvAltura.setText(cliente?.altura.toString())
+                binding.tvSexo.setText(cliente?.sexo.toString())
+                binding.tvPorcentajeGrasa.setText(cliente?.porcentajeGrasaCorporal.toString())
+                binding.tvPorcentajeMusculo.setText(cliente?.porcentajeDeMusculo.toString())
+                binding.imageProfile.setImageURI(usuario?.photoUrl)
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Error :(")
+            }
+
+        })
     }
 
     private fun configurarEventos() {
@@ -50,6 +97,33 @@ class HomeFragment : Fragment() {
             AuthUI.getInstance().signOut(requireContext())
             startActivity(Intent(activity,loginActivity::class.java))
         }
+        binding.btnActualizarPerfil.setOnClickListener{
+            ActualizarDatos()
+        }
+    }
+
+    private fun ActualizarDatos() {
+        val baseDatos = Firebase.database
+        val usuario = FirebaseAuth.getInstance().currentUser
+        //Se actualiza cada vez que me llegue un dato
+        //Clientes
+        val referencia = baseDatos.getReference("Clientes/${usuario?.uid}")
+        //val id = baseDatos.getReference("Clientes/${usuario?.uid}/id").get()
+        val valor = Cliente(
+            usuario?.uid.toString(),
+            binding.tvNombre.text.toString(),
+            binding.tvEmail.text.toString(),
+            binding.tvPeso.text.toString().toDouble(),
+            binding.tvAltura.text.toString().toDouble(),
+            binding.tvSexo.text.toString(),
+            binding.tvPorcentajeMusculo.text.toString().toDouble(),
+            binding.tvPorcentajeGrasa.text.toString().toDouble(),
+        0,
+        )
+        referencia.setValue(valor)
+
+
+
     }
 
     override fun onDestroyView() {
